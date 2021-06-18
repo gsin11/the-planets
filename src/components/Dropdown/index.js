f75fc4dc8dcf67418c0d03e8ecae5f94ff5f9a97
Gurpreet Singh <gurupreetsingh2000@gmail.com>
2021-06-18 20:46:29 +0200

    Update index.js

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-const Dropdown = ({ data, isOpen }) => {
+const Dropdown = ({ data, isOpen, onClickHandler }) => {
   return (
     isOpen && (
       <div className="grid grid-rows-4 text-center items-center bg-black py-2 gap-2 relative z-10">
         {data.map((o) => (
           <Link key={o.id} href={`/${o.slug}`}>
-            <a className="ml-4 uppercase font-semibold text-lg">{o.name}</a>
+            <a className="ml-4 uppercase font-semibold text-lg" onClick={() => onClickHandler()}>{o.name}</a>
           </Link>
         ))}
       </div>
